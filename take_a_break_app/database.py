2619dd06f42109b61e60a0bb178ae88d028cb8bc
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-05-15 03:02:15 -0400

    add function for register page

--- take_a_break_app/database.py
+++ take_a_break_app/database.py
@@ -4,6 +4,40 @@ This file contain all database operation for take_a_break_app
 
 import pymysql
 
-db = pymysql.connect(host='localhost', 
-                        password="gfqpsmp123",
-                        database='')
+def create_user(username, email, password):
+    """
+    create a user with the given information, validation of information will be done before this
+    create_user: string, string, string -> Bool
+    """
+    db = pymysql.connect(host='localhost', 
+                    user = "root",
+                    password="gfqpsmp123",
+                    database='login')
+    cursor = db.cursor()
+    command = "INSERT INTO login (username, email, password) VALUES (\"{}\", \"{}\", \"{}\")".format(username, email, password)
+    try:
+        cursor.execute(command)
+        db.commit()
+    except:
+        db.rollback()
+        return False #shows a error to backend
+    db.close()
+    return True # shows add successfully
+
+
+
+
+if __name__ == "__main__":
+    print("Testing Databse \n")
+    db = pymysql.connect(host='localhost', 
+                    user = "root",
+                    password="gfqpsmp123",
+                    database='login')
+    cursor = db.cursor()
+    cursor.execute("SELECT VERSION()")
+    data = cursor.fetchone()
+    print ("Database version : %s " % data)
+    db.close()
+
+
+    print(create_user("test", "test", "test"))

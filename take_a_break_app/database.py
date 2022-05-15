ef64aee4d853172d547c59f191d503d116b6a1d3
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-05-14 23:46:59 -0400

    bug fix

---
+++ take_a_break_app/database.py
@@ -0,0 +1,9 @@
+"""
+This file contain all database operation for take_a_break_app
+"""
+
+import pymysql
+
+db = pymysql.connect(host='localhost', 
+                        password="gfqpsmp123",
+                        database='')

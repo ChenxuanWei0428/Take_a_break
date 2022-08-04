ca04de1a4025988a685027671f22fc0d2462afce
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-08-03 23:39:57 -0400

    finish core product

--- take_a_break_app/static/take_a_break_app/break.js
+++ take_a_break_app/static/take_a_break_app/break.js
@@ -1,6 +1,16 @@
 function time_after_redirect(time) {
-    current_url = window.location.host+"main";
-    console.log(current_url);
-    console.log(time);
+    target_url = "http://"+window.location.host+"/main";
+    break_time = get_total_second(time);
+    console.log(break_time);
+    console.log(target_url);
     
+    var timer = setTimeout(function() {
+        window.location.href=target_url;
+    }, break_time*1000);
+    
+}
+
+function get_total_second(time) {
+    // time should be in format of hh:mm:ss
+    return Number(time.slice(0,2))*3600+Number(time.slice(3, 5))*60+Number(time.slice(6, 8));
 }

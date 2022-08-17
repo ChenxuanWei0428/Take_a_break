21438679bc9db8828ef7e5adfe69a39efdda34da
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-08-16 23:57:32 -0400

    try frontend validation

---
+++ take_a_break_app/static/take_a_break_app/register.js
@@ -0,0 +1,6 @@
+function validateForm() {
+    let username = forms["register"]["username"].value;
+    let email = forms["register"]["email"].value;
+    let password = forms["register"]["password"].value;
+    let confirm_password = forms["register"]["confirm_password"].value;
+}

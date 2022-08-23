93e9b47a77cbad8424263989469f216cbf80e4f3
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-08-23 02:33:19 -0400

    finish email frontend validation

--- take_a_break_app/static/take_a_break_app/register.js
+++ take_a_break_app/static/take_a_break_app/register.js
@@ -1,4 +1,5 @@
 function validateForm() {
+    console.log("check")
     // please refer to views.py for changes
     let username = forms["register"]["username"].value;
     let email = forms["register"]["email"].value;
@@ -13,41 +14,22 @@ function validateForm() {
         error_message = "Email is too long";
     } else if (password.length > 100) {
         error_message = "Password is too long";
-    }  else {
-        
+    }  else if (!valid_email(email)) {
+        error_message = "Incorrect email format";
     }
-    /*
-    if (password != confirmed_password):
-            return 2
-        if len(username)> 100:
-            return 4
-        if len(email)> 100:
-            return 5
-        pattern = "^\S+@\S+\.\S+$"
-        if len(password)> 100:
-            return 6
-        if not check_user_exist(username):
-            return 3 
-        if not valid_email(email):
-            return 7
-        return 0
-    else:
-        return 1
-    */
-   /*
-   elif response_id == 1:
-            error_message = "Please enter valid character"
-        elif response_id == 2:
-            error_message = "Please double check your password"
-        elif response_id == 3:
-            error_message =  "User already exist"
-        elif response_id == 4:
-            error_message = "Username is too long"
-        elif response_id == 5:
-            error_message = "Email is too long"
-        elif response_id == 6:
-            error_message = "Password is too long"
-        elif response_id == 7:
-            error_message = "Incorrect email format"
-   */
+    if (error_message == "") {
+        return true;
+    } else {
+        ReactDOM.render(
+            error_message,
+            document.getElementById('error_message')
+        );
+        console.log("test")
+        return false;
+    }
+}
+
+function valid_email(email) {
+    let pattern = /^\S+@\S+\.\S+$/i;
+    return email.search(pattern) != -1;
 }
